1d26ff876e0e2c77928df6dd32fcaafdd3d8d17d
Amir Mustafa <amirengg15@gmail.com>
2024-01-21 14:24:26 +0530

    feat: JWT token auth module configure, signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,12 +9,15 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private jwtService: JwtService
   ) {}
 
   async signUp(authcredentialsDto: AuthCredentialsDto): Promise<void> {
@@ -41,12 +44,15 @@ export class AuthService {
     }
   }
 
-  async signIn(authcredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(authcredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
     const { username, password } = authcredentialsDto;
     const user = await this.userRepository.findOne({ select: ["id", "username", "password"], where: {username} });
 
     if(user && await bcrypt.compare(password, user.password)) {
-        return "login successful";
+        // JWT TOKEN FOR SECURE
+        const payload: JwtPayload = { username };
+        const accessToken = this.jwtService.sign(payload);
+        return { accessToken };
     } else {
         throw new UnauthorizedException('Please check your login credentials.');
     }
