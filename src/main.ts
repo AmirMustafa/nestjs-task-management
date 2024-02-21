5b572d3f15eb4170bb2b3d37973dba6b12aa43da
Amir Mustafa <amirengg15@gmail.com>
2024-02-21 15:46:16 +0530

    logger: displaying app port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
